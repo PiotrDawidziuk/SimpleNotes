e6afeedfa14597ceb2d5ba5574746209ee46df62
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-03-25 22:18:41 +0100

    unnecessary imports deleted

--- app/src/main/java/pl/piotrdawidziuk/simplenotes/MainActivity.java
+++ app/src/main/java/pl/piotrdawidziuk/simplenotes/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
