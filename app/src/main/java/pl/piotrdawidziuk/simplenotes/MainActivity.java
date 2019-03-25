df9b7d6f73a9aeb99463fe57e2101080033b746f
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-03-25 17:04:02 +0100

    on text changed

--- app/src/main/java/pl/piotrdawidziuk/simplenotes/MainActivity.java
+++ app/src/main/java/pl/piotrdawidziuk/simplenotes/MainActivity.java
@@ -1,19 +1,42 @@
 package pl.piotrdawidziuk.simplenotes;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
 
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
         editText = findViewById(R.id.editText);
-        
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
